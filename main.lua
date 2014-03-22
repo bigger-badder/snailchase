d7ca380f8d7bd64bd2e1c80ae8a58c29ed31c075
Marcell Jusztin <jusztin.marcell@gmail.com>
2014-03-22 15:43:55 +0000

    Added MTE lib

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
--- Dirt bike run
+-- Dirt Bike Run
+--
+-- Copyright (c) Elliot Agro, Marcell Jusztin 2014.  
+--
+-- v1.0
+
+
+-- Libraries
+local mte = require("lib.mte").createMTE()
+
+
+-- App settings
+display.setStatusBar( display.HiddenStatusBar )
